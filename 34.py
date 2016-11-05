f02f2a12299b6b680d55314b013463228aef2593
Kassen <tkassen@SDGL1428723df.local>
2016-11-04 19:24:31 -0700

    Added 30+

---
+++ 34.py
@@ -0,0 +1,9 @@
+from Bio import pairwise2
+from Bio.SubsMat.MatrixInfo import blosum62
+from fastamasta import FastaReader
+
+if __name__ == "__main__":
+    data = [i[1] for i in FastaReader("data/34.fas")]
+
+    for i in pairwise2.align.globalds(data[0],data[1], blosum62, -5, -0):
+        print pairwise2.format_alignment(*i)
