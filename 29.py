aec51a407dd72ad6ec142598d20073cfcd0e2d56
Ted Kassen <tedgkassen@gmail.com>
2016-11-04 19:19:06 -0700

    Added 29

--- 29.py
+++ 29.py
@@ -1,7 +1,9 @@
-from sequence import hamming_dist
-
 if __name__ == "__main__":
     data_file = open("data/29.dat", 'r')
     A,B = data_file.readline().strip(), data_file.readline().strip()
 
-    print hamming_dist(A, B)
+    dist = 0
+    for i in range(len(A)):
+        if A[i] != B[i]:
+            dist += 1
+    print dist
